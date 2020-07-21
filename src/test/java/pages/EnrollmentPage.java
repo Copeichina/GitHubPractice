ff6cb7c3b2611acc1428eca73b5f13c4c97e045a
Copeichina <olga.greencard@gmail.com>
2020-07-21 15:40:49 -0400

    added the resources and page

---
+++ src/test/java/pages/EnrollmentPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class EnrollmentPage {
+    public static void main(String[] args) {
+        System.out.println("created enrollment page");
+    }
+}
---
+++ src/test/resources/Enrollment.feature
@@ -0,0 +1,5 @@
+Feature: I am able to enroll
+
+  Scenario: User is able to enroll
+    When I am on the enroll page
+    And I click enroll button
