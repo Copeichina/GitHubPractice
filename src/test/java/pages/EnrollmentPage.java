5efdd32f372bf7d5cf3d965d4a42cf62f2630360
Copeichina <olga.greencard@gmail.com>
2020-07-21 15:44:52 -0400

    added the resources and page modified

--- src/test/java/pages/EnrollmentPage.java
+++ src/test/java/pages/EnrollmentPage.java
@@ -2,6 +2,7 @@ package pages;
 
 public class EnrollmentPage {
     public static void main(String[] args) {
-        System.out.println("created enrollment page");
+        System.out.println("created enrollment ");
+        System.out.println("another change");
     }
 }
--- src/test/resources/Enrollment.feature
+++ src/test/resources/Enrollment.feature
@@ -3,3 +3,5 @@ Feature: I am able to enroll
   Scenario: User is able to enroll
     When I am on the enroll page
     And I click enroll button
+    When I enter user information
+    Then I am able to enroll
