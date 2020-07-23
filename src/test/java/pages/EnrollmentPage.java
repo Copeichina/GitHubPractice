b8f52a19bb1532f8b4d44c0f453746af7a558ce4
Copeichina <57867741+Copeichina@users.noreply.github.com>
2020-07-23 11:33:47 -0400

    Update EnrollmentPage.java

--- src/test/java/pages/EnrollmentPage.java
+++ src/test/java/pages/EnrollmentPage.java
@@ -4,5 +4,8 @@ public class EnrollmentPage {
     public static void main(String[] args) {
         System.out.println("created enrollment ");
         System.out.println("another change");
+        System.out.println("from master remote");
+        System.out.println("another");
+        System.out.println("another change");
     }
 }
